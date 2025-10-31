717b3767e40dc581eae4ab49a0d7a8c020134a8a
Amirhossein Mohammadi <accretence@outlook.com>
2025-10-31 23:36:28 +0330

    local environment

--- next/src/middleware.ts
+++ next/src/middleware.ts
@@ -1,27 +1,27 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
-  const isAuthRoute = req.nextUrl.pathname.includes("/login");
-  const token = req.cookies.get("access_token")?.value; // adjust cookie name for Django session
+  const isAuthRoute = req.nextUrl.pathname.includes('/login')
+  const token = req.cookies.get('access_token')?.value
 
   // If not logged in and trying to access a protected page
   if (!token && !isAuthRoute) {
-    const loginUrl = new URL("/login", req.url);
-    return NextResponse.redirect(loginUrl);
+    const loginUrl = new URL('/login', req.url)
+    return NextResponse.redirect(loginUrl)
   }
 
   // If logged in and trying to access login page
   if (token && isAuthRoute) {
-    const homeUrl = new URL("/", req.url);
-    return NextResponse.redirect(homeUrl);
+    const homeUrl = new URL('/', req.url)
+    return NextResponse.redirect(homeUrl)
   }
 
-  return NextResponse.next();
+  return NextResponse.next()
 }
 
 export const config = {
   matcher: [
-    "/((?!api|static|.*\\..*|_next|favicon.ico|sitemap.xml|robots.txt|login).*)",
+    '/((?!api|static|.*\\..*|_next|favicon.ico|sitemap.xml|robots.txt|login).*)',
   ],
-};
+}
